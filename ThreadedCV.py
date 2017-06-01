82a3355aee9509365e2385a122df21a648a5db99
Mike Vanis <hello@mikevanis.com>
2017-06-01 14:56:56 +0100

    added threaded capture

--- ThreadedCV.py
+++ ThreadedCV.py
@@ -8,9 +8,9 @@ import time
 import cv2
 
 camera = PiCamera()
-camera.resolution = (640,480)
+camera.resolution = (320,240)
 camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(640,480))
+rawCapture = PiRGBArray(camera, size=(320,240))
 stream = camera.capture_continuous(rawCapture, format="bgr", use_video_port=True)
 
 fps = FPS().start()
@@ -32,3 +32,18 @@ print("Approx. FPS: {:.2f}".format(fps.fps()))
 stream.close()
 rawCapture.close()
 camera.close()
+
+vs = PiVideoStream().start()
+time.sleep(2.0)
+fps = FPS().start()
+while fps._numFrames < 100:
+    frame = vs.read()
+    frame = imutils.resize(frame, width=400)
+
+    fps.update()
+
+fps.stop()
+print("Elapsed time: {:.2f}".format(fps.elapsed()))
+print("Approx. FPS: {:.2f}".format(fps.fps()))
+
+vs.stop()
